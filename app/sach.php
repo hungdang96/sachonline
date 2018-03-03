4d86f7a255a957c3fd9c3e06857cb780e641c047
vandiepbui <35633954+vandiepbui@users.noreply.github.com>
2018-03-03 20:20:50 +0700

    acv

--- app/sach.php
+++ app/sach.php
@@ -11,7 +11,9 @@ class sach extends Model
 
     protected $table = 'sach';
     protected $fillable = ['s_sku','s_ten','s_giaGoc','s_giaBan','s_soTrang','s_namXuatBan','s_tao','s_capNhat','s_kichThuoc','s_danhGia','s_loaiBia','s_trangThai','s_gioiThieu','nxb_maFK','tl_maFK','nn_maFK'];
+
     protected $primaryKey = 's_sku';
+    
     protected $dates = ['s_tao', 's_capNhat', 's_namXuatBan'];
     protected $dateFormat = 'Y-m-d H:i:s';
 }
