544a1a99ddc0879a3f84ad4b4df26aea3bf6144b
hungdang96 <35732943+hungdang96@users.noreply.github.com>
2018-03-04 11:58:17 +0700

    Models

--- app/khachhang.php
+++ app/khachhang.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class khachhang extends Model
 {
-    //
+    const CREATED_AT = 'kh_taoMoi';
+    const UPDATED_AT = 'kh_capNhat';
+
+    protected $table = 'khachhang';
+    protected $fillable = ['kh_ma','kh_taiKhoan','kh_matKhau','kh_hoTen','kh_gioiTinh','kh_email','kh_ngaySinh','kh_diaChi','kh_soDienThoai','kh_taoMoi','kh_capNhat','kh_trangThai'];
+
+    protected $dates = ['kh_taoMoi','kh_capNhat','kh_ngaySinh'];
+    protected $dateFormat = 'Y-m-d H:i:s';
 }
