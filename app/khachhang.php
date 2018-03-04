f8ff583bd6dc998bbc87c6bb2ad3acdd74d825b9
hungdang96 <35732943+hungdang96@users.noreply.github.com>
2018-03-04 14:05:08 +0700

    khmodel

--- app/khachhang.php
+++ app/khachhang.php
@@ -12,6 +12,7 @@ class khachhang extends Model
     protected $table = 'khachhang';
     protected $fillable = ['kh_ma','kh_taiKhoan','kh_matKhau','kh_hoTen','kh_gioiTinh','kh_email','kh_ngaySinh','kh_diaChi','kh_soDienThoai','kh_taoMoi','kh_capNhat','kh_trangThai'];
 
+    protected $primaryKey = 'kh_ma';
     protected $dates = ['kh_taoMoi','kh_capNhat','kh_ngaySinh'];
     protected $dateFormat = 'Y-m-d H:i:s';
 }
