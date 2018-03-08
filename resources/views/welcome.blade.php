a3bd99f5aee7f90f83746b713e0436ee36c1217f
hungdang96 <35732943+hungdang96@users.noreply.github.com>
2018-03-08 20:47:38 +0700

    Controllers

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,17 +9,16 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-        <link href="{!! asset('css/app.css') !!}">
+        {{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">--}}
+        <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
+        <link rel="stylesheet" href="{!! asset('css/font-awesome.min.css') !!}">
+        {{--<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>--}}
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        {{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>--}}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
         <script src="{!! asset('js/app.js') !!}"></script>
     </head>
     <body>
-        <nav class="navbar navbar-expand-sm navbar-light">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link"><i class=""></i></a>
-                </li>
-            </ul>
-        </nav>
+
     </body>
 </html>
