a303e71ecb75bf4a6d0069a658b26f35930c321f
hungdang96 <35732943+hungdang96@users.noreply.github.com>
2018-03-13 00:15:56 +0700

    controller 16-23

--- app/Http/Controllers/PhieunhapController.php
+++ app/Http/Controllers/PhieunhapController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\phieunhap;
+use function compact;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use function json_encode;
 
 class phieunhapController extends Controller
 {
@@ -13,7 +17,9 @@ class phieunhapController extends Controller
      */
     public function index()
     {
-        //
+        $ds_phieunhap = phieunhap::all();
+        $json = json_encode($ds_phieunhap);
+        return response(['error'=>false,'message'=>compact('ds_phieunhap','json')],200);
     }
 
     /**
@@ -23,7 +29,7 @@ class phieunhapController extends Controller
      */
     public function create()
     {
-        //
+        return View('welcome');
     }
 
     /**
@@ -34,7 +40,22 @@ class phieunhapController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $phieunhap = new phieunhap();
+        $phieunhap->pn_ma = $request->pn_ma;
+        $phieunhap->pn_nguoiGiao = $request->pn_nguoiGiao;
+        $phieunhap->pn_soHoaDon = $request->pn_soHoaDon;
+        $phieunhap->pn_ngayXuatHoaDon = $request->pn_ngayXuatHoaDon;
+        $phieunhap->pn_nguoiLapPhieu = $request->pn_nguoiLapPhieu;
+        $phieunhap->pn_ngayNhapPhieu = $request->pn_ngayNhapPhieu;
+        $phieunhap->nv_keToan = $request->nv_keToan;
+        $phieunhap->pn_ngayXacNhan = $request->pn_ngayXacNhan;
+        $phieunhap->nv_thuKho = $request->nv_thuKho;
+        $phieunhap->pn_ngayNhapKho = $request->pn_ngayNhapKho;
+        $phieunhap->pn_trangThai = $request->pn_trangThai;
+        $phieunhap->nph_maFK = $request->nph_maFK;
+        $phieunhap->save();
+
+        return response(['error'=>false,'message'=>$phieunhap->toJson()],200);
     }
 
     /**
@@ -56,7 +77,11 @@ class phieunhapController extends Controller
      */
     public function edit($id)
     {
-        //
+        $phieunhap = phieunhap::where('pn_ma',$id)->first();
+        $result = ['error'=> $phieunhap == null,
+                    'message'=>($phieunhap == null?"Không tìm thấy phiếu nhập phieunhap[{$id}]":$phieunhap->toJson())];
+        return View('welcome',['result'=>$result]);
+
     }
 
     /**
@@ -68,7 +93,25 @@ class phieunhapController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $phieunhap = phieunhap::where('pn_ma',$id)->first();
+        if($phieunhap){
+            $phieunhap->pn_nguoiGiao = $request->pn_nguoiGiao;
+            $phieunhap->pn_soHoaDon = $request->pn_soHoaDon;
+            $phieunhap->pn_ngayXuatHoaDon = $request->pn_ngayXuatHoaDon;
+            $phieunhap->pn_nguoiLapPhieu = $request->pn_nguoiLapPhieu;
+            $phieunhap->pn_ngayNhapPhieu = $request->pn_ngayNhapPhieu;
+            $phieunhap->nv_keToan = $request->nv_keToan;
+            $phieunhap->pn_ngayXacNhan = $request->pn_ngayXacNhan;
+            $phieunhap->nv_thuKho = $request->nv_thuKho;
+            $phieunhap->pn_ngayNhapKho = $request->pn_ngayNhapKho;
+            $phieunhap->pn_trangThai = $request->pn_trangThai;
+            $phieunhap->nph_maFK = $request->nph_maFK;
+            $phieunhap->save();
+
+            return response(['error'=>false,'message'=>$phieunhap->toJson()],200);
+        }else{
+            return response(['error'=> true, 'message'=>"Không tìm thấy phiếu nhập phieunhap[{$id}]"],200);
+        }
     }
 
     /**
@@ -79,6 +122,12 @@ class phieunhapController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $phieunhap = phieunhap::where('pn_ma',$id)->first();
+        if($phieunhap){
+            $phieunhap->delete();
+            return response(['error'=>false,'message' => "Đã xóa phieunhap[{$id}]"],200);
+        }else{
+            return response(['error'=>true,'message'=>"Không tìm thấy phiếu nhập phieunhap[{$id}]"],200);
+        }
     }
 }
